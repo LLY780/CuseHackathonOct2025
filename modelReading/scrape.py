cb7fc3e0e1232d56c0b8c9b483f11e6adcc75f0e
Thomas Allen <thomasallen6706@gmail.com>
2025-10-26 12:31:26 -0400

    Made things work! - Changed file structure

--- modelReading/scrape.py
+++
@@ -1,7 +0,0 @@
-import trafilatura
-
-def getText(url):
-    '''Download and extract text from article url'''
-    downloaded = trafilatura.fetch_url(url)
-    article_text = trafilatura.extract(downloaded)
-    return article_text
