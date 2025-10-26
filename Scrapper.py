e851530ce0b7def7d8f48ae91568deb4b8ecf4eb
noobhell304 <39669794+noobhell304@users.noreply.github.com>
2025-10-25 21:27:34 -0400

    Add files via upload

--- Scrapper.py
+++ Scrapper.py
@@ -1,10 +1,7 @@
 import trafilatura
-
-url = "https://www.cnn.com/2025/10/25/business/trump-tariffs-canada-reagan"
-
 # Download and extract main text
-downloaded = trafilatura.fetch_url(url)
-article_text = trafilatura.extract(downloaded)
-
-
-print(article_text)
+def getText(x):
+    url = x
+    downloaded = trafilatura.fetch_url(url)
+    article_text = trafilatura.extract(downloaded)
+    return article_text
